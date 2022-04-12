c87fc9671df4bdffbce9603d3f8cccfd0cbac18c
joel <joelkalema63@gmail.com>
2022-04-13 01:32:59 +0200

    Added the content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import './style.css';
 
-fetch('https://api.tvmaze.com/search/shows?q=spiderman')
-  .then((respose) => respose.json())
-  .then((jsonData) => jsonData);
+const displayItem = (results) => {
+  const card = document.querySelector('.cards');
+  card.innerHTML = '';
+  results.forEach((item) => {
+    const scoreLi = document.createElement('div');
+    scoreLi.className = 'card';
+    scoreLi.innerHTML = `<p class="movi-title">${item.name}</p>
+                         <img src="${item.image.medium}">
+                         <button type="submit" class="reservation-btn">Reservation</button>`;
+    card.appendChild(scoreLi);
+  });
+};
 
-fetch('https://api.tvmaze.com/shows')
-  .then((respose) => respose.json())
-  .then((jsonData) => console.log(jsonData));
+const searchShow = (query) => {
+  const BASE_URL = `https://api.tvmaze.com/search/shows?q=${query}`;
+  fetch(BASE_URL)
+    .then((respose) => respose.json())
+    .then((jsonData) => {
+      const results = jsonData.map((item) => item.show);
+      displayItem(results);
+      console.log(results)
+    });
+};
+
+window.onload = () => {
+  const searchArea = document.querySelector('.Search');
+  searchArea.onkeyup = (e) => {
+    searchShow(searchArea.value);
+  };
+};
