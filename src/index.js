221ce2925cc54a480030978357cddc2cf46828c6
joel <joelkalema63@gmail.com>
2022-04-12 16:52:00 +0200

    Modied the file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import './style.css';
 fetch('https://api.tvmaze.com/search/shows?q=spiderman')
   .then((respose) => respose.json())
   .then((jsonData) => jsonData);
+
+fetch('https://api.tvmaze.com/shows')
+  .then((respose) => respose.json())
+  .then((jsonData) => console.log(jsonData));
