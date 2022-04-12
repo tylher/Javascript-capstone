ce0ccbcff8a0524f3a3c49e569b0e029b5feb04f
joel <joelkalema63@gmail.com>
2022-04-12 12:09:51 +0200

    Added the IPA

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import _ from 'lodash';
 import './style.css';
+
+fetch("https://api.tvmaze.com/search/shows?q=spiderman")
+.then((respose) => {
+  return respose.json();
+})
+.then((jsonData) => {
+  console.log(jsonData);
+})
