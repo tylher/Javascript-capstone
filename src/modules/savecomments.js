a8efc96ef7c7f2512fef180048cee1d1153c8d92
Damola Adediran <tylher123@gmail.com>
2022-04-14 21:07:06 +0100

    Create methods to save and get comments

---
+++ src/modules/savecomments.js
@@ -0,0 +1,27 @@
+const BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const appId = 'jvrwtpOKv3EF9dzf4scl';
+
+const saveComment = async ({ id, name, comment }) => {
+  const save = await fetch(`${BASE_URL}/apps/${appId}/comments`, {
+    method: 'POST',
+    body: [
+      JSON.stringify({
+        item_id: id,
+        username: name,
+        comment,
+      }),
+    ],
+  }).then((resp) => console.log(resp.json()));
+
+  return save;
+};
+
+const getComments = async (uniqueId) => {
+  const data = await fetch(
+    `${BASE_URL}/apps/${appId}/comments?items_id=${uniqueId}`,
+  ).then((res) => res.json());
+
+  return data;
+};
+
+export { saveComment, getComments };
