3352dd69e51ae898d5da9e0c4310a8465efea022
joel <joelkalema63@gmail.com>
2022-04-14 15:28:42 +0200

    Real version fo items

--- src/index.js
+++ src/index.js
@@ -1,49 +1,36 @@
 import './style.css';
-import './popup.css';
-import { temp, renderPopUp } from './modules/popup.js';
-import getShow from './modules/get-show.js';
 
-const like = false;
 const displayItem = (results) => {
   const card = document.querySelector('.cards');
   card.innerHTML = '';
   results.forEach((item) => {
     const scoreLi = document.createElement('div');
     scoreLi.className = 'card';
-    scoreLi.innerHTML = `<p class="movi-title">${item.name}</p>
-                         <img src="${item.image.medium}">
-                         <button type="submit" class="reservation-btn">Reservation</button>`;
+    scoreLi.innerHTML = `<img src="${item.image.medium}">
+                         <p class="movi-title">${item.name}</p>
+                         <div>
+                         <button type="submit" class="comment-btn">Comment</button>
+                         ${like ? '<i class="fa-solid fa-heart"></i>' : '<i class="fa-regular fa-heart"></i>'}
+                         </div>`;
     card.appendChild(scoreLi);
   });
 };
 
-const searchShow = (query) => {
+const searchShow = async (query) => {
   const BASE_URL = `https://api.tvmaze.com/search/shows?q=${query}`;
-  fetch(BASE_URL)
+  await fetch(BASE_URL)
     .then((respose) => respose.json())
     .then((jsonData) => {
       const results = jsonData.map((item) => item.show);
       displayItem(results);
-      const commentBtn = document.querySelectorAll('.comment-btn');
-      commentBtn.forEach((comment) => {
-        comment.addEventListener('click', (e) => {
-          const NAME = e.target.parentElement.parentElement.childNodes[0].textContent;
-          results.map((item) => {
-            if (NAME === item.name) {
-              displayPopUp(item.id);
-            }
-            return '';
-          });
-        });
-      });
     })
     .catch(() => {
       displayItem([]);
-    })
+    });
 };
 
 const acceil = document.querySelector('.title');
-const searchArea = document.querySelector('.search');
+const searchArea = document.querySelector('.Search');
 let setTimeoutTOken = 0;
 window.onload = () => {
   clearTimeout(setTimeoutTOken);
@@ -52,14 +39,13 @@ window.onload = () => {
       return;
     }
 
-    if(searchArea.value !== ''){
+    if (searchArea.value !== '') {
       acceil.classList.add('none');
-      acceil.classList.remove('block')
-    }else {
-      acceil.classList.add('block')
-      acceil.classList.remove('none')
+      acceil.classList.remove('block');
+    } else {
+      acceil.classList.add('block');
+      acceil.classList.remove('none');
     }
-    console.log(searchArea.value)
     setTimeoutTOken = setTimeout(() => {
       searchShow(searchArea.value);
     }, 250);
@@ -73,15 +59,16 @@ const displayhomeItem = (result) => {
     const scoreLi = document.createElement('div');
     scoreLi.className = 'card';
     scoreLi.innerHTML = `<img src="${item.image.medium}">
-                         <p class="movi-title">${item.name}</p>`;
+                         <p class="movi-title">${item.name}</p>
+                         <button type="submit" class="reservation-btn">Comment</button>`;
     card.appendChild(scoreLi);
   });
 };
 
-const BASE_URL = 'https://api.tvmaze.com/search/shows?q=blac';
-fetch(BASE_URL)
+const BASE_URL = `https://api.tvmaze.com/search/shows?q=blac`;
+  fetch(BASE_URL)
   .then((respose) => respose.json())
   .then((jsonData) => {
-    const result = jsonData.map((item) => item.show);
+   const result = jsonData.map((item) => item.show);
     displayhomeItem(result);
-  });
+    })
--- src/modules/get-show.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = 'https://api.tvmaze.com';
-const getShow = async (id) => {
-  const show = await fetch(`${BASE_URL}/shows/${id}`).then((resp) => resp.json());
-  return show;
-};
-export default getShow;
