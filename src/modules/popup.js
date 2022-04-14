9ea2d222f005052dfb0779dd417485893a86c420
Damola Adediran <tylher123@gmail.com>
2022-04-14 13:27:28 +0100

    Add button to form

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -11,59 +11,63 @@ const temp = ({
           <h2>${name}</h2>
           ${summary}
           <div class="movie-details">
-          <div class="flex-grp">
-            <div>
-              <h4>Genres :</h4>
-              <ul class="genres">
-                ${genres.map((genre) => `
-                <li>${genre}</li>
-                `).join('')}
-              </ul>
-            </div>
-            <div>
-              <h4>Rating :</h4>
-              <div class="star-rating">
-                <div class="outer-star-rating">
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                </div>
-                <div class="inner-star-rating " style="width:${rating.average * 10}%">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
+            <div class="flex-grp">
+              <div>
+                <h4>Genres :</h4>
+                <ul class="genres">
+                  ${genres.map((genre) => `
+                  <li>${genre}</li>
+                  `).join('')}
+                </ul>
+              </div>
+              <div>
+                <h4>Rating :</h4>
+                <div class="star-rating">
+                  <div class="outer-star-rating">
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                  </div>
+                  <div
+                    class="inner-star-rating"
+                    style="width:${rating.average * 10}%"
+                  >
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="flex-grp">
-            <div>
-              <h4>Language :</h4>
-              <p>${language}</p>
+            <div class="flex-grp">
+              <div>
+                <h4>Language :</h4>
+                <p>${language}</p>
+              </div>
+              <div>
+                <h4>Status :</h4>
+                <p>${status}</p>
+              </div>
             </div>
-            <div>
-              <h4>Status :</h4>
-              <p>${status}</p>
-            </div>
-          </div>
           </div>
           <section class="comments">
-          <div class="show-comments">
-          <h3>Comments</h3>
-          <ul class='comments-holder'></ul>
-          </div>
-          <div class="add-comment">
-            <h3>Add new comment</h3>
-            <form>
-             <input type="text" place-holder="Your name">
-             <textarea palace-holder="Your insight" ></textarea>
-            </form>
-          </div>
-         <section>
+            <div class="show-comments">
+              <h3>Comments</h3>
+              <ul class="comments-holder"></ul>
+            </div>
+            <div class="add-comment">
+              <h3>Add new comment</h3>
+              <form>
+                <input type="text" placeholder="Your name" />
+                <textarea placeholder="Your insight"></textarea>
+                <button type="submit">Comment</button>
+              </form>
+            </div>
+          </section>
         </div>
       </div>
   `;
